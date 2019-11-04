71187852c0f936a2707f159112834b57b06b8101
Mikey Clarke <epicschnozz@gmail.com>
2019-11-04 13:34:56 +1300

    Moar

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,8 +42,6 @@ set :rbenv_ruby, '2.6.5'
 # http://stackoverflow.com/questions/34126546
 set :rbenv_path, '/home/app-user/.rbenv'
 
-set :stage, :production
-
 before :deploy, 'git:add'
 before :deploy, 'git:commit'
 before :deploy, 'git:push'
