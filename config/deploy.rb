a3e17eb188fc3b119b4f4bed1824937434090486
Mikey Clarke <pineutrino@gmail.com>
2025-09-20 11:19:02 +1200

    Moar

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,5 +44,4 @@ set :ssh_options, { forward_agent: true }
 #set :rbenv_type, :user
 set :rbenv_ruby, '3.4.3'
 set :rbenv_path, '/home/noob/.rbenv'
-set :rbenv_map_bins, %w{rake gem bundle ruby}
-set :rbenv_roles, :all
+set :bundle_binstubs, -> { shared_path.join('bin') }
